e6953ae911bb9ced380c70b9070b687121000db8
Jorge Eduardo Ardila <jeam@tuta.io>
2021-08-23 09:52:40 -0500

    feat: changed project tree

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
+
+function App() {
+	const [ token, setToken ] = useState();
+  return (
+    <div className="wrapper">
+		<h1>Apps</h1>
+		<BrowserRouter>
+			<Switch>
+				<Route path="/dashboard">
+					<Dashboard />
+				</Route>
+				<Route path="/preferences">
+					<Preferences />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
